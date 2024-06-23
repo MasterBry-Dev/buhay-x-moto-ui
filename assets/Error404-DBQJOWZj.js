08743f5b5027bd6509d381365d53b081f6aa6f01
BryanSurio <113846848+MasterBry-Dev@users.noreply.github.com>
2024-06-23 11:45:09 +0800

    Updates

---
+++ assets/Error404-DBQJOWZj.js
@@ -0,0 +1,9 @@
+import{u as a,r as o,s as n,a as i,j as e,L as l}from"./index-DC6MTuy5.js";const c=()=>{const r=a();o.useEffect(()=>{r(n("Error 404"))});const s=i(t=>t.themeConfig.theme==="dark"||t.themeConfig.isDarkMode);return e.jsx("div",{className:"relative flex min-h-screen items-center justify-center overflow-hidden",children:e.jsx("div",{className:`px-6 py-16 \r
+            text-center font-semibold \r
+            before:container \r
+            before:absolute \r
+            before:left-1/2 \r
+            before:-translate-x-1/2 \r
+            before:rounded-full \r
+            before:bg-[linear-gradient(180deg,#4361EE_0%,rgba(67,97,238,0)_50.73%)] \r
+            before:aspect-square before:opacity-10 md:py-20`,children:e.jsxs("div",{className:"relative",children:[e.jsx("img",{src:s?"/buhay-x-moto-ui/assets/images/error/maintenence-dark.svg":"/buhay-x-moto-ui/assets/images/error/maintenence-light.svg",alt:"404",className:"mx-auto -mt-10 w-full max-w-xs object-cover md:-mt-14 md:max-w-xl"}),e.jsx("p",{className:"mt-5 text-xl text-dark dark:text-white font-bold",children:"COMING SOON...."}),e.jsx(l,{to:"/",className:"text-dark dark:text-white-light text-xl text-bold btn btn-gradient mx-auto !mt-7 w-max border-0 uppercase shadow-none",children:"Home"})]})})})};export{c as default};
